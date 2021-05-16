29d4329d8c8fc13804677f11c0e4d53857b98e44
Sangone Jung <sangonejung@Sangones-MBP.lan1>
2021-05-15 23:21:44 -0500

    Updated POST request & Edited Get Request

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+//Middleware
+app.use(express.json())
+
 //Temp Database for rooms
 const rooms = [
     { name: "playPoker", password: "12345"},
@@ -8,22 +11,39 @@ const rooms = [
     { name: "unclePoker", password: "rdr2letsgo"}
 ]
 
-//Basic Get Request 
+//Default Get Request 
 app.get('/', (req,res) => {
     res.send('Hello World')
 })
 
-//GET RoomName Request (Room Routing)
-//Password Validation has not been built yet
+//GET Room Name & Password Request
 app.get('/api/room/:name/:password', (req,res) =>{
-    let roomname = req.params.name
-    rooms.forEach(function(index){
-        if(roomname == index.name){
-            res.send(`Welcome to ${roomname}, please wait until the game starts`)
-        }
-    })
-    // res.send(`${roomname} does not exist in our database. Please try again`)
+    const room = rooms.find(r => r.name === req.params.name)
+    if (!room) res.status(404).send(`The following ${req.params.name} has not been found. Please try again`)
+    if(room.password != req.params.password){
+        res.status(404).send(`Incorrect Password. Please try again`)
+    }
+    res.send(`Welcome to ${room.name}, please wait until the game starts`)
 })
+
+//POST Room Name & Password Request
+app.post('/api/room', (req,res) => {
+    //Input Validation
+    if(!req.body.name || !req.body.password){
+        res.status(400).send("Name or Password Required")
+        return
+    } else if(rooms.find(r => r.name === req.body.name)){
+        res.status(400).send(`${req.body.name} has already been created.`)
+        return
+    }
+    const room = {
+        name: req.body.name,
+        password: req.body.password
+    }
+    rooms.push(room)
+    res.send(`${room.name} has been created. Please click the button below if you wish to start the game`)
+    console.log(room.password)
+}) 
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Running on ${port}`))
